456f7f5d49abf4cde0d23e268cb40b0f379b0bad
Jedidiah Alfred-Aigbe <jedidiah.alfred-aigbe@stu.ocsb.ca>
2023-02-08 19:38:34 +0000

    added stylechecker

---
+++ HelloWorldStyle.java
@@ -0,0 +1,19 @@
+/**
+* This program prints my plans for next year.
+*
+* @author Jedidiah Alfred
+* @version 1.0
+* @since 2023-02-08
+*/
+class HelloWorldStyle {
+    /**
+    * This program prints my plans for next year.
+    *
+    * @param args Unused
+    */
+    public static void main(String[] args) {
+        System.out.print("Next year, I plan to be settled down ");
+        System.out.print("in York University over in Toronto and i'm excited!");
+        System.out.println(" for this experience!");
+    }
+}
